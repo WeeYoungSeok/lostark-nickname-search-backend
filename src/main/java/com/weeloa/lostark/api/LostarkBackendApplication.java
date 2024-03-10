2374b5137b6d34b0ed37a304637b5797190ea6af
WeeYoungSeok <dndrldzk@gmail.com>
2024-03-10 13:06:59 +0900

    feat: setting
    springboot setting

---
+++ src/main/java/com/weeloa/lostark/api/LostarkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.weeloa.lostark.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostarkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostarkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weeloa/lostark/api/LostarkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weeloa.lostark.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostarkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
